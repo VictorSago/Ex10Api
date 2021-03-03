4535d325a67df387f6d35d83b9986bd321d57d76
Victor Sago <VictorSago01@gmail.com>
2021-03-03 15:44:51 +0100

    Can switch to a new deck now

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,23 +2,32 @@
 let baseUri = 'https://deckofcardsapi.com/api/deck/';
 
 let drawCardButton = document.querySelector("#draw-card");
+let newDeckButton = document.querySelector('#new-deck');
+let shuffleDeckButton = document.querySelector('#shuffle-deck');
 let cardPictureSpace = document.querySelector("#card-pic");
 
-drawCardButton.addEventListener('click', getCard);
-
 let deckId = 'new';
+
+newDeckButton.addEventListener('click', () => {
+    console.log('Previous deck ID: ', deckId);
+    deckId = 'new';
+    cardPictureSpace.textContent = '';
+})
+
+drawCardButton.addEventListener('click', getCard);
 
 function getCard() {
     let actionCmd = 'draw';
     let numberOfCards = 1;
     let uri = baseUri + deckId + "/" + actionCmd + "/?count=" + numberOfCards;
-    console.log(uri);
+    //console.log(uri);
     fetch(uri)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             let imgSrc = data.cards[0].image;
-            console.log(imgSrc);
+            deckId = data.deck_id;
+            console.log(deckId);
             const imgElement = document.createElement('img');
             imgElement.setAttribute('src', imgSrc);
             cardPictureSpace.textContent = '';
