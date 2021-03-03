f07512cf658c32d66a7de7434600684c29a7bc3e
Victor Sago <VictorSago01@gmail.com>
2021-03-03 16:47:10 +0100

    Using Bootstrap4 card class to display the image of a card

--- scripts/cards.js
+++ scripts/cards.js
@@ -9,29 +9,55 @@ let cardPictureSpace = document.querySelector("#card-pic");
 let deckId = 'new';
 
 newDeckButton.addEventListener('click', () => {
-    console.log('Previous deck ID: ', deckId);
     deckId = 'new';
+    // Clear the contents of the card viewing area
     cardPictureSpace.textContent = '';
 })
 
 drawCardButton.addEventListener('click', getCard);
 
 function getCard() {
-    let actionCmd = 'draw';
-    let numberOfCards = 1;
+    let actionCmd = 'draw'; // For when we need other actions
+    let numberOfCards = 1;  // In case we want to expand in the future to fetching multiple cards at once
     let uri = baseUri + deckId + "/" + actionCmd + "/?count=" + numberOfCards;
-    //console.log(uri);
     fetch(uri)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            let imgSrc = data.cards[0].image;
+            //console.log(data);
+            // Get the deck ID and the URL to the card image
             deckId = data.deck_id;
-            console.log(deckId);
+            let imgSrc = data.cards[0].image;
+
+            // Header of the card div element
+            let cardName = `${data.cards[0].value} of ${data.cards[0].suit}`;
+            const cardHeader = document.createElement('h5');
+            cardHeader.classList.add('card-header');
+            cardHeader.classList.add('text-center');
+            cardHeader.innerText = cardName;
+
+            // Card image
             const imgElement = document.createElement('img');
             imgElement.setAttribute('src', imgSrc);
+            imgElement.classList.add('card-img');
+            imgElement.setAttribute('alt', `Picture of the ${cardName}`);
+            
+            // Body of the card div element
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            cardBody.innerHTML = [
+                `<ul class="list-group list-group-flush">`,
+                    `<li class="list-group-item">Deck ID: ${data.deck_id}</li>`,
+                    `<li class="list-group-item">Cards remaining: ${data.remaining}</li>`,
+                `</ul>`
+            ].join('\n');
+            
+            // Clear all previous contents
             cardPictureSpace.textContent = '';
+
+            // Add new HTML to the card container
+            cardPictureSpace.appendChild(cardHeader);
             cardPictureSpace.appendChild(imgElement);
+            cardPictureSpace.appendChild(cardBody);
         })
         .catch((err) => {
             console.error(err);
