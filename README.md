a011861494999a7fa58032f8badcbc460cfe108e
Victor Sago <VictorSago01@gmail.com>
2021-03-03 11:43:33 +0100

    Finished StarWars character fetch biometrics

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Fetches biometric data for various StarWars characters.
 
 ### Part 2 of Exercise 10 in Lexicon's .NET course
 
+***UNFINISHED***
+
 Draws a card from a deck of playing cards using API of a webservice.
---
+++ scripts/starwars.js
@@ -0,0 +1,51 @@
+let searchBtn = document.querySelector("#search-button");
+let searchField = document.querySelector("#search-field");
+let outputText = document.querySelector("#output-area");
+
+searchBtn.addEventListener("click", fetchBio);
+
+// Clear the search and the output fields
+searchField.value = "";
+outputText.value = "";
+
+searchField.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      //event.preventDefault();
+      // Trigger the button with a click
+      if (searchField.value) {
+          searchBtn.click();
+      }
+    }
+  }); 
+
+function fetchBio() {
+    if (searchField.value) {
+        let uri = "https://www.swapi.tech/api/people/?name=" + searchField.value;
+        console.log(uri);
+        fetch(uri)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            let output = "";
+            if (data.result.length > 0) {
+                data.result.forEach(item => {
+                    let charBio = [
+                        `Name: ${item.properties.name},`,
+                        `Gender: ${item.properties.gender},`,
+                        `Hair Colour: ${item.properties.hair_color},`,
+                        `Height: ${item.properties.height} cm,`,
+                        `Weight: ${item.properties.mass} kg;`
+                    ].join('\n');
+                    output += charBio + '\n\n';
+                });
+            } else {
+                output = "No such character.";
+            }
+            outputText.value = output;
+        })
+        .catch((err) => {
+            console.error(err);
+            outputText.value = "Something went wrong...";
+        });
+    }
+}
